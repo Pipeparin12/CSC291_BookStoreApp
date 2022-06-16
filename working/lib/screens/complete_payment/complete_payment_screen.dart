0f4922765235b26bd18b733c4892d3c2b4e78587
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-16 20:44:56 +0700

    Mole

--- working/lib/screens/complete_payment/complete_payment_screen.dart
+++ working/lib/screens/complete_payment/complete_payment_screen.dart
@@ -10,7 +10,7 @@ class CompletePaymentScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Fill the info'),
       ),
-      body: Sody(),
+      body: Body(),
     );
   }
 }

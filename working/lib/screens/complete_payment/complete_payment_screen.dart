0b1ca8306f8f547858b83af1f57194ce0a7f5c8c
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-16 20:36:03 +0700

    fix

--- working/lib/screens/complete_payment/complete_payment_screen.dart
+++ working/lib/screens/complete_payment/complete_payment_screen.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'components/body.dart';
 
 class CompletePaymentScreen extends StatelessWidget {
-  static String routeName = "/complete_Payment";
+  static String routeName = "/complete_payment";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fill up'),
+        title: Text('Fill the info'),
       ),
       body: Body(),
     );

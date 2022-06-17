9283c39b6add961c39f5647e57ae10824a303eff
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-17 19:57:21 +0700

    add phone route

--- working/lib/routes.dart
+++ working/lib/routes.dart
@@ -4,12 +4,14 @@ import 'package:flutter/widgets.dart';
 import 'package:shop_app/screens/cart/cart_screen.dart';
 import 'package:shop_app/screens/complete_email/complete_email_screen.dart';
 import 'package:shop_app/screens/complete_payment/complete_payment_screen.dart';
+import 'package:shop_app/screens/complete_phone/complete_phone_screen.dart';
 import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
 import 'package:shop_app/screens/details/details_screen.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
 import 'package:shop_app/screens/otp/otp_screen.dart';
+import 'package:shop_app/screens/otp_phone/otp_screen.dart';
 import 'package:shop_app/screens/profile/profile_screen.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
@@ -26,10 +28,12 @@ final Map<String, WidgetBuilder> routes = {
   SignUpScreen.routeName: (context) => SignUpScreen(),
   CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
   OtpScreen.routeName: (context) => OtpScreen(),
+  OtpPhoneScreen.routeName: (context) => OtpPhoneScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
   CartScreen.routeName: (context) => CartScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
   CompletePaymentScreen.routeName: (context) => CompletePaymentScreen(),
   CompleteEmailScreen.routeName: (context) => CompleteEmailScreen(),
+  CompletePhoneScreen.routeName: (context) => CompletePhoneScreen(),
 };

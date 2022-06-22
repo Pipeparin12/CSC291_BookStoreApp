8f414c2d175512d2f2354d1068898fbc6f7c00fb
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-22 17:39:43 +0700

    Change profile icon

--- working/lib/screens/profile/components/profile_pic.dart
+++ working/lib/screens/profile/components/profile_pic.dart
@@ -24,18 +24,13 @@ class ProfilePic extends StatelessWidget {
             child: SizedBox(
               height: 46,
               width: 46,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    side: BorderSide(color: Colors.white),
-                  ),
-                  primary: Colors.white,
-                  backgroundColor: Color(0xFFF5F6F9),
-                ),
-                onPressed: () {},
-                child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
-              ),
+              child: InkWell(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.teal,
+                    size: 28.0,
+                  )),
             ),
           )
         ],

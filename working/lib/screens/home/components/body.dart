3f756e953cb769c2b4c7f06f9b553069bb876faa
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-24 15:02:18 +0700

    Delete unused

--- working/lib/screens/home/components/body.dart
+++ working/lib/screens/home/components/body.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../size_config.dart';
-import 'categories.dart';
-//import 'discount_banner.dart';
+
 import 'home_header.dart';
-// import 'popular_product.dart';
-import 'special_offers.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -15,14 +12,7 @@ class Body extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: getProportionateScreenHeight(20)),
-            HomeHeader(),
-            SizedBox(height: getProportionateScreenWidth(10)),
-            // DiscountBanner(),
-            Categories(),
-            SpecialOffers(),
-            // SizedBox(height: getProportionateScreenWidth(30)),
-            // PopularProducts(),
-            SizedBox(height: getProportionateScreenWidth(30)),
+            HomeHeader()
           ],
         ),
       ),

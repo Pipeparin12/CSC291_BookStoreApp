a451790cbfb21d84265cc0c73742dae9238bd457
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-24 17:01:12 +0700

    M. Checkout page

--- working/lib/screens/Chechout/components/check_out_cards.dart
+++ working/lib/screens/Chechout/components/check_out_cards.dart
@@ -37,6 +37,28 @@ class CheckoutsCard extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: getProportionateScreenWidth(40),
+                  width: getProportionateScreenWidth(40),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF5F6F9),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SvgPicture.asset("assets/icons/receipt.svg"),
+                ),
+                Spacer(),
+                Text("Add voucher code"),
+                const SizedBox(width: 10),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 12,
+                  color: kTextColor,
+                )
+              ],
+            ),
             SizedBox(height: getProportionateScreenHeight(20)),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -55,7 +77,7 @@ class CheckoutsCard extends StatelessWidget {
                 SizedBox(
                   width: getProportionateScreenWidth(190),
                   child: DefaultButton(
-                    text: "Comfirm Payment",
+                    text: "comfirm payment",
                     press: () {},
                   ),
                 ),

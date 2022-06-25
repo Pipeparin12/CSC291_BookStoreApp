0f4026600373b3c31ce4275e0fdc11b3f7416f9f
JPthanasan <jackpot2545@gmail.com>
2022-06-25 19:11:13 +0700

    .

--- bookstoreapp291/lib/screen/add_book.dart
+++ bookstoreapp291/lib/screen/add_book.dart
@@ -1,4 +1,4 @@
-import 'package:bookstoreapp291/size_config.dart';
+import 'package:bookstoreapp291/sizedConfig.dart';
 import 'package:bookstoreapp291/theme/light_color.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';

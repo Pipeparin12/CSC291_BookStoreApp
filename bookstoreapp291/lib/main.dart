10eff7fa63ed3476f23eebbaeb9ddd4a8dcfaad9
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-28 03:29:44 +0700

    .

--- bookstoreapp291/lib/main.dart
+++ bookstoreapp291/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookstoreapp291/screen/sign_in/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:bookstoreapp291/routes.dart';
 import 'package:bookstoreapp291/screen/profile/profile_screen.dart';
 import 'package:bookstoreapp291/screen/splash/splash_screen.dart';
@@ -8,10 +9,16 @@ import 'package:bookstoreapp291/size_config.dart';
 import 'package:bookstoreapp291/theme.dart';
 import 'package:bookstoreapp291/widget/bottomNavBar.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCP7aV-ug8cSLUCJG5CqaaMVO5oPgViu10", // Your apiKey
+      appId: "1:484998251170:web:bdbf04ddf58ee78c954dfb", // Your appId
+      messagingSenderId: "484998251170", // Your messagingSenderId
+      projectId: "book-store-app-291", // Your projectId
+    ),
+  );
   runApp(MyApp());
 }
 

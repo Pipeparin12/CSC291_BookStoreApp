0e00876409e6e0bb69f6469db8530ab45fb8e00e
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-28 02:22:38 +0700

    .

--- bookstoreapp291/lib/main.dart
+++ bookstoreapp291/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookstoreapp291/screen/sign_in/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:bookstoreapp291/routes.dart';
 import 'package:bookstoreapp291/screen/profile/profile_screen.dart';
 import 'package:bookstoreapp291/screen/splash/splash_screen.dart';
@@ -9,7 +8,10 @@ import 'package:bookstoreapp291/size_config.dart';
 import 'package:bookstoreapp291/theme.dart';
 import 'package:bookstoreapp291/widget/bottomNavBar.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 

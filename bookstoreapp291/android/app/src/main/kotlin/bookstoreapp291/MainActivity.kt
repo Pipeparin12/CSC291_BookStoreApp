f821661cc741aa50f137d1b2321032a7913fb301
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-27 18:46:06 +0700

    .

--- bookstoreapp291/android/app/src/main/kotlin/bookstoreapp291/MainActivity.kt
+++ bookstoreapp291/android/app/src/main/kotlin/bookstoreapp291/MainActivity.kt
@@ -1,5 +1,3 @@
 package bookstoreapp291
-import android.os.Bundle;
-    import io.flutter.app.FlutterActivity;
-    import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity
     public class MainActivity extends FlutterActivity {

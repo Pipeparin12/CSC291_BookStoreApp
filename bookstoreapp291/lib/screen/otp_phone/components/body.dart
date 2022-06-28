7ae794d5e99cb8c5961674dbf43131170171c673
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-28 20:49:31 +0700

    commit2

---
+++ bookstoreapp291/lib/screen/otp_phone/components/body.dart
@@ -0,0 +1,65 @@
+import 'package:bookstoreapp291/screen/otp_phone/otp_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:bookstoreapp291/constants.dart';
+import 'package:bookstoreapp291/sizedConfig.dart';
+
+import 'otpphone_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.05),
+              Text(
+                "OTP Verification",
+                style: headingStyle,
+              ),
+              Text("We sent your code to +66 94 870 ****"),
+              buildTimer(),
+              OtpPhoneForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => OtpPhoneScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Resend OTP Code",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildTimer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("This code will expired in "),
+        TweenAnimationBuilder(
+          tween: Tween(begin: 60.0, end: 0.0),
+          duration: Duration(seconds: 60),
+          builder: (_, dynamic value, child) => Text(
+            "00:${value.toInt()}",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}

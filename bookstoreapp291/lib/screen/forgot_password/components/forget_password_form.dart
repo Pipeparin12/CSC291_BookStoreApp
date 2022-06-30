4195da25ca15cb6cc542b739ade4c00ea17c8333
xXAchilleSXx <ch.werakit@outlook.com>
2022-06-30 17:18:59 +0700

    .

---
+++ bookstoreapp291/lib/screen/forgot_password/components/forget_password_form.dart
@@ -0,0 +1,55 @@
+import 'dart:html';
+import 'package:bookstoreapp291/screen/sign_in/sign_in_screen.dart';
+import 'package:bookstoreapp291/screen/sign_up/components/sign_up_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:bookstoreapp291/components/custom_surfix_icon.dart';
+import 'package:bookstoreapp291/components/default_button.dart';
+import 'package:bookstoreapp291/components/from_error.dart';
+import 'package:bookstoreapp291/components/no_account_text.dart';
+import 'package:bookstoreapp291/sizedConfig.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import '../../../constants.dart';
+
+class ForgotPassForm extends StatefulWidget {
+  @override
+  _ForgotPassFormState createState() => _ForgotPassFormState();
+}
+
+class _ForgotPassFormState extends State<ForgotPassForm> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> errors = [];
+  String? email;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "Email",
+              hintText: "Enter your email",
+              // If  you are using latest version of flutter then lable text and hint text shown like this
+              // if you r using flutter less then 1.20.* then maybe this is not working properly
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+            ),
+          ),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          FormError(errors: errors),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          DefaultButton(
+            text: "Continue",
+            press: () {},
+            onPressed: () {},
+          ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          NoAccountText(),
+        ],
+      ),
+    );
+  }
+}

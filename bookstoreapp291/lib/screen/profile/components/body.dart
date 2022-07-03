53682f4cdc012388210926fb913c0dba8e1d127b
Pipeparin12 <79441231+Pipeparin12@users.noreply.github.com>
2022-07-04 00:03:56 +0700

    merge with Nile

--- bookstoreapp291/lib/screen/profile/components/body.dart
+++ bookstoreapp291/lib/screen/profile/components/body.dart
@@ -1,10 +1,18 @@
+import 'package:bookstoreapp291/AddProfile.dart';
+import 'package:bookstoreapp291/screen/profile/components/profile_detail.dart';
+import 'package:bookstoreapp291/screen/profile/profile_screen.dart';
+import 'package:bookstoreapp291/screen/sign_in/sign_in_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:bookstoreapp291/screen/complete_address/complete_address_screen.dart';
 import 'package:bookstoreapp291/screen/complete_email/complete_email_screen.dart';
+
 import 'package:bookstoreapp291/screen/complete_payment/complete_payment_screen.dart';
 import 'package:bookstoreapp291/screen/complete_phone/complete_phone_screen.dart';
 import 'package:bookstoreapp291/screen/complete_profile/complete_profile_screen.dart';
 
+import 'profile_detail.dart';
 import 'profile_menu.dart';
 import 'profile_pic.dart';
 
@@ -15,43 +23,20 @@ class Body extends StatelessWidget {
       padding: EdgeInsets.symmetric(vertical: 20),
       child: Column(
         children: [
-          ProfilePic(),
+          ProfilePic(title: ''),
           SizedBox(height: 20),
+          ProfileDetail(this),
           ProfileMenu(
-            text: "My Account",
-            icon: "assets/icons/User Icon.svg",
+            text: "Edit Profile",
+            icon: "assets/icons/User.svg",
             press: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => CompleteProfileScreen())),
+                context, MaterialPageRoute(builder: (context) => AddProfile())),
           ),
           ProfileMenu(
-            text: "Payment",
-            icon: "assets/icons/Bill Icon.svg",
-            press: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => CompletePaymentScreen())),
-          ),
-          ProfileMenu(
-            text: "Email",
-            icon: "assets/icons/Mail.svg",
+            text: "Sign Out",
+            icon: "assets/icons/Log out.svg",
             press: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => CompleteEmailScreen())),
-          ),
-          ProfileMenu(
-            text: "Phone Number",
-            icon: "assets/icons/Phone.svg",
-            press: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => CompletePhoneScreen())),
-          ),
-          ProfileMenu(
-            text: "Address",
-            icon: "assets/icons/Discover.svg",
-            press: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => CompleteAddressScreen())),
+                MaterialPageRoute(builder: (context) => SignInScreen())),
           ),
         ],
       ),
--- bookstoreapp291/lib/screen/profile/components/profile_pic.dart
+++ bookstoreapp291/lib/screen/profile/components/profile_pic.dart
@@ -1,11 +1,61 @@
+import 'dart:io';
+
 import 'package:bookstoreapp291/theme/light_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class ProfilePic extends StatelessWidget {
-  const ProfilePic({
-    Key? key,
-  }) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+class ProfilePic extends StatefulWidget {
+  const ProfilePic({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<ProfilePic> createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+  File? Images;
+
+  Future pickImage() async {
+    try {
+      final Images = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+      if (Images == null) return;
+
+      final imageTemp = File(Images.path);
+
+      setState(() => this.Images = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageC() async {
+    try {
+      final Images = await ImagePicker().pickImage(source: ImageSource.camera);
+
+      if (Images == null) return;
+
+      final imagePermanent = await saveImagePermanently(Images.path);
+      setState(() => this.Images = imagePermanent);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future<File> saveImagePermanently(String imagePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final name = basename(imagePath);
+    final Images = File('${directory.path}/$name');
+
+    return File(imagePath).copy(Images.path);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +66,9 @@ class ProfilePic extends StatelessWidget {
         fit: StackFit.expand,
         clipBehavior: Clip.none,
         children: [
-          CircleAvatar(
-            backgroundImage: AssetImage("assets/images/Profile Image.png"),
-          ),
+          Images != null
+              ? ClipOval(child: Image.file(Images!, fit: BoxFit.cover))
+              : FlutterLogo(),
           Positioned(
             right: -16,
             bottom: 0,
@@ -42,39 +92,43 @@ class ProfilePic extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget BottomSheet() {
-  return Container(
-    height: 100.0,
-    margin: EdgeInsets.symmetric(
-      horizontal: 20,
-      vertical: 20,
-    ),
-    child: Column(
-      children: <Widget>[
-        Text(
-          "Choose Profile photo",
-          style: TextStyle(
-            fontSize: 20.0,
+  Widget BottomSheet() {
+    return Container(
+      height: 100.0,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Choose Profile photo",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.camera),
-            onPressed: () {},
-            label: Text("Camera"),
+          SizedBox(
+            height: 20,
           ),
-          FlatButton.icon(
-            icon: Icon(Icons.image),
-            onPressed: () {},
-            label: Text("Gallery"),
-          ),
-        ])
-      ],
-    ),
-  );
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.camera),
+              onPressed: () {
+                pickImageC();
+              },
+              label: Text("Camera"),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                pickImage();
+              },
+              label: Text("Gallery"),
+            ),
+          ])
+        ],
+      ),
+    );
+  }
 }

ad461010183fe3121fe7dde5387f9e1c07c4e45b
xXAchilleSXx <ch.werakit@outlook.com>
2022-07-03 18:23:10 +0700

    add

--- bookstoreapp291/lib/AddProfile.dart
+++ bookstoreapp291/lib/AddProfile.dart
@@ -1,5 +1,6 @@
 import 'package:bookstoreapp291/theme/light_color.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -54,6 +55,7 @@ class _AddProfileState extends State<AddProfile> {
         FirebaseFirestore.instance.collection('User').doc(NameId);
 
     Map<String, dynamic> User = ({
+      "UserId": FirebaseAuth.instance.currentUser!.email,
       "NameId": NameId,
       "firstName": firstName,
       "lastName": lastName,
--- bookstoreapp291/lib/screen/profile/components/body.dart
+++ bookstoreapp291/lib/screen/profile/components/body.dart
@@ -1,5 +1,7 @@
+import 'package:bookstoreapp291/AddProfile.dart';
 import 'package:bookstoreapp291/screen/profile/profile_screen.dart';
 import 'package:bookstoreapp291/screen/sign_in/sign_in_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:bookstoreapp291/screen/complete_address/complete_address_screen.dart';
@@ -18,9 +20,11 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  String login = '';
-  Widget showLogin() {
-    return Text('Name: $login');
+  Future addDetail() async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    var currentUser = _auth.currentUser;
+    CollectionReference _collectionRef =
+        FirebaseFirestore.instance.collection("User");
   }
 
   @override
@@ -29,44 +33,13 @@ class _BodyState extends State<Body> {
       padding: EdgeInsets.symmetric(vertical: 20),
       child: Column(
         children: [
-          showLogin(),
           ProfilePic(title: ''),
           SizedBox(height: 20),
           ProfileMenu(
-            text: "My Account",
-            icon: "assets/icons/User Icon.svg",
+            text: "Add Profile",
+            icon: "assets/icons/User.svg",
             press: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => CompleteProfileScreen())),
-          ),
-          ProfileMenu(
-            text: "Payment",
-            icon: "assets/icons/Bill Icon.svg",
-            press: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => CompletePaymentScreen())),
-          ),
-          ProfileMenu(
-            text: "Email",
-            icon: "assets/icons/Mail.svg",
-            press: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => CompleteEmailScreen())),
-          ),
-          ProfileMenu(
-            text: "Phone Number",
-            icon: "assets/icons/Phone.svg",
-            press: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => CompletePhoneScreen())),
-          ),
-          ProfileMenu(
-            text: "Address",
-            icon: "assets/icons/Discover.svg",
-            press: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => CompleteAddressScreen())),
+                context, MaterialPageRoute(builder: (context) => AddProfile())),
           ),
           ProfileMenu(
             text: "Sign Out",
